13ffcd478558513e259b0286fd0a4619823fb44f
David Gregory <42326109+hunoz@users.noreply.github.com>
2023-07-14 17:41:23 -0600

    First deployment

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -10,7 +10,7 @@ resource "aws_secretsmanager_secret" "bifrost_api_user_key_secret" {
   name = "BifrostApiIamUser"
 }
 
-resource "aws_secretsmanager_secret_version" "marron_api_user_key_secret_version" {
+resource "aws_secretsmanager_secret_version" "bifrost_api_user_key_secret_version" {
   secret_id     = aws_secretsmanager_secret.bifrost_api_user_key_secret.id
   secret_string = jsonencode({ "AccessKeyId" : aws_iam_access_key.bifrost_api_user_key.id, "SecretAccessKey" : aws_iam_access_key.bifrost_api_user_key.secret })
 }
